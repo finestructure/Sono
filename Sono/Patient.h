371b4529c520c6472535d11d8322023566f35f25
Sven A. Schmidt <sas@abstracture.de>
2012-02-27 16:40:04 +0100

    Added Patient model

---
+++ Sono/Patient.h
@@ -0,0 +1,13 @@
+//
+//  Patient.h
+//  Sono
+//
+//  Created by Sven A. Schmidt on 27.02.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface Patient : NSManagedObject
+
+@end
---
+++ Sono/Patient.m
@@ -0,0 +1,13 @@
+//
+//  Patient.m
+//  Sono
+//
+//  Created by Sven A. Schmidt on 27.02.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "Patient.h"
+
+@implementation Patient
+
+@end
