fe1e1da0b1de6806d559c0a71fe7dca1f843a500
Sven A. Schmidt <sas@abstracture.de>
2012-02-28 09:56:47 +0100

    Added dropdown button skeleton

---
+++ Sono/DropdownButton.h
@@ -0,0 +1,13 @@
+//
+//  DropdownButton.h
+//  Sono
+//
+//  Created by Sven A. Schmidt on 28.02.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DropdownButton : UIButton
+
+@end
---
+++ Sono/DropdownButton.m
@@ -0,0 +1,31 @@
+//
+//  DropdownButton.m
+//  Sono
+//
+//  Created by Sven A. Schmidt on 28.02.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "DropdownButton.h"
+
+@implementation DropdownButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
