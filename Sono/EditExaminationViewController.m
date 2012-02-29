3b5f12423b6b3f18c6e8bd646b3e5d34c3519954
Sven A. Schmidt <sas@abstracture.de>
2012-02-29 14:03:37 +0100

    Added edit examination layout & view controller

---
+++ Sono/EditExaminationViewController.m
@@ -0,0 +1,43 @@
+//
+//  EditExaminationViewController.m
+//  Sono
+//
+//  Created by Sven A. Schmidt on 29.02.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import "EditExaminationViewController.h"
+
+@interface EditExaminationViewController ()
+
+@end
+
+@implementation EditExaminationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+	return YES;
+}
+
+@end
