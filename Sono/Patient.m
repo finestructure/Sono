a2fa5a605857cfb0177e0cd1b2433d21a346bca0
Sven A. Schmidt <sas@abstracture.de>
2012-02-29 12:36:39 +0100

    Added validation to Patient (needs to be handled!)

--- Sono/Patient.m
+++ Sono/Patient.m
@@ -28,4 +28,30 @@
   self.examinations = tempSet;
 }
 
+
+- (BOOL)validateBirthDate:(__autoreleasing id *)value error:(NSError *__autoreleasing *)error
+{
+  NSDate *date = *value;
+  if (date == nil) {
+    if (error != nil) {
+      NSString *errorStr = @"Geburtsdatum ist Pflichtfeld";
+      NSDictionary *userInfoDict = [NSDictionary dictionaryWithObject:errorStr forKey:NSLocalizedDescriptionKey];
+      NSError *e = [[NSError alloc] initWithDomain:@"Patient" code:1 userInfo:userInfoDict];
+      *error = e;
+    }
+    return NO;
+  }
+  if ([[NSDate date] timeIntervalSinceDate:date] < 0) {
+    if (error != nil) {
+      NSString *errorStr = @"Geburtsdatum muss in der Vegangenheit liegen";
+      NSDictionary *userInfoDict = [NSDictionary dictionaryWithObject:errorStr forKey:NSLocalizedDescriptionKey];
+      NSError *e = [[NSError alloc] initWithDomain:@"Patient" code:2 userInfo:userInfoDict];
+      *error = e;
+    }
+    return NO;
+  }
+  return YES;
+}
+
+
 @end
