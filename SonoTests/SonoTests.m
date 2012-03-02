1078940e085f0defb7e66fdad30946f84e885ea3
Sven A. Schmidt <sas@abstracture.de>
2012-03-02 12:58:31 +0100

    Added parser test

--- SonoTests/SonoTests.m
+++ SonoTests/SonoTests.m
@@ -8,29 +8,35 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
+#import "CSVParser.h"
+
 @interface SonoTests : SenTestCase
 @end
 
 
 @implementation SonoTests
 
-- (void)setUp
-{
-    [super setUp];
-    
-    // Set-up code here.
+
+- (void)setUp {
+  [super setUp];
 }
 
-- (void)tearDown
-{
-    // Tear-down code here.
-    
-    [super tearDown];
+
+- (void)tearDown {
+  [super tearDown];
 }
 
-- (void)testExample
-{
-    STFail(@"Unit tests are not implemented yet in SonoTests");
+
+- (void)test_01_parser {
+  NSString *test = @"A;B;C\n1;2;3\n";
+  CSVParser *parser = [[CSVParser alloc] initWithString:test separator:@";" hasHeader:YES fieldNames:nil];
+  __block int count = 0;
+  [parser parseRowsUsingBlock:^(NSDictionary *row) {
+    count++;
+    STAssertEqualObjects([row objectForKey:@"A"], @"1", nil);
+    STAssertEqualObjects([row objectForKey:@"B"], @"2", nil);
+    STAssertEqualObjects([row objectForKey:@"C"], @"3", nil);
+  }];
 }
 
 @end
