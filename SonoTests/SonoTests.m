15c25e06e5f02e818e9b510b3b1839e0aecc04e2
Sven A. Schmidt <sas@abstracture.de>
2012-03-02 12:25:46 +0100

    Added unit tests target

---
+++ SonoTests/SonoTests.m
@@ -0,0 +1,36 @@
+//
+//  SonoTests.m
+//  SonoTests
+//
+//  Created by Sven A. Schmidt on 02.03.12.
+//  Copyright (c) 2012 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SonoTests : SenTestCase
+@end
+
+
+@implementation SonoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SonoTests");
+}
+
+@end
